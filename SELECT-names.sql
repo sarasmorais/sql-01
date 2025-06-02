db5b01d4b75577358de0d63efdd4692ea21f2fef
S A R A <123466976+sarasmorais@users.noreply.github.com>
2025-06-02 16:33:29 -0300

    Create SELECT-names.sql

---
+++ SELECT-names.sql
@@ -0,0 +1,27 @@
+SELECT name FROM world
+  WHERE name LIKE 'y%'
+
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+SELECT name FROM world
+  WHERE name LIKE '%X%'
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+SELECT name FROM world
+  WHERE name LIKE 'c%ia'
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+SELECT name FROM world
+WHERE name LIKE '%a%a%a%'
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
